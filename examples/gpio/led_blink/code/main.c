1630c0dd78abddf25fc69ec01724e24af0e05173
chasegunjal <96733492+chasegunjal@users.noreply.github.com>
2026-09-09 19:59:07 +0530

    written LED blink code
    Corrected header file inclusions and defined LED pin for GPIO control.

--- examples/gpio/led_blink/code/main.c
+++ examples/gpio/led_blink/code/main.c
@@ -1,1 +1,15 @@
+#include "driver/gpio.g"
+#incllude "freertos/FreeRTOS.h"
+#incllude "freertos/task.h"
 
+#define LED_pin GPIO_NUM_2
+void app_main(void){
+  gpio_set_direction(LED_pin, GPIO_MODE_OUTPUT);
+
+  while(1){
+    gpio_set_level(LED_pin,1);
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    gpio_set_level(LED_pin,0);
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+}
